2b651dd4e17abab0b7160be52799d3f6d4faad51
iiparraguirreodaw1 <iiparraguirreodaw1@gmail.com>
2021-04-27 18:01:26 +0200

    parsearLinea() terminado

--- src/ut7/agenda/io/AgendaIO.java
+++ src/ut7/agenda/io/AgendaIO.java
@@ -1,4 +1,5 @@
 package ut7.agenda.io;
+
 import ut7.agenda.modelo.AgendaContactos;
 import ut7.agenda.modelo.Contacto;
 import ut7.agenda.modelo.Personal;
@@ -7,10 +8,11 @@ import ut7.agenda.modelo.Relacion;
 
 /**
  * Utilidades para cargar la agenda
+ * 
  * @author Iker Iparraguirre, Jon García, Naiara Borrega
  */
 public class AgendaIO {
-	
+
 	/**
 	 * Este método importa todos los contactos que se obtienen del metodo<br>
 	 * obtenerLineasDatos().
@@ -18,25 +20,30 @@ public class AgendaIO {
 	 * @param agenda - Agenda donde se importaran los contactos
 	 */
 	public static void importar(AgendaContactos agenda) {
+
 		for (int i = 0; i < obtenerLineasDatos().length; i++) {
 			agenda.añadirContacto(parsearLinea(obtenerLineasDatos()[i]));
 		}
 	}
 
-	private static Contacto parsearLinea(String linea) {
+	private static Contacto parsearLinea(String linea) throws NumberFormatException {
+
+		// NumberFormatException es la excepcion que debe de saltar..
+
 		String[] split = linea.split(",");
 		for (int i = 0; i < split.length; i++) {
 			split[i] = split[i].trim();
 		}
-			
+
 		if (split[0].equals("1")) {
 			Profesional p = new Profesional(split[1], split[2], split[3], split[4], split[5]);
 			return p;
 		} else {
-			Personal p = new Personal(split[1], split[2], split[3], split[4], 
-					                  split[5], Relacion.valueOf(split[6].toUpperCase()));
+			Personal p = new Personal(split[1], split[2], split[3], split[4], split[5],
+					Relacion.valueOf(split[6].toUpperCase()));
 			return p;
 		}
+
 	}
 
 }
